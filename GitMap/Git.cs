73c6dc5a43cbc452809884c6e6cf916d9cbc73c4
Dara Keon <lc.sales.duarte@gmail.com>
2019-07-19 20:31:17 -0300

    gitmap: add mapping github repos
    List all and create a script to recreate them in another machine

---
+++ GitMap/Git.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace GitMap
+{
+	class Git
+	{
+		public Git(String path)
+		{
+			Path = path;
+
+			var configPath = System.IO.Path.Combine(path, ".git", "config");
+			var config = File.ReadAllText(configPath);
+
+			var regex = new Regex(@"git@github\.com:.+\/.+\.git");
+			Url = regex.Match(config).Groups[0].Value;
+		}
+
+		public String Path { get; }
+		public String Url { get; }
+
+		public override string ToString()
+		{
+			return $"mkdir {Path}\n" +
+				$"cd {Path}\n" +
+				$"git clone {Url} .\n";
+		}
+	}
+}
---
+++ GitMap/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace GitMap
+{
+	class Program
+	{
+		public static void Main(String[] args)
+		{
+			Console.WriteLine("Hey, listen!");
+
+			var gits = getAllGit(args[0]);
+
+			gits.ToList().ForEach(Console.WriteLine);
+
+			Console.ReadLine();
+		}
+
+		private static Git[] getAllGit(String path)
+		{
+			var directories = Directory.GetDirectories(path);
+
+			var gitDir = directories.SingleOrDefault(
+				d => d.EndsWith(@"\.git")
+			);
+
+			if (gitDir != null)
+			{
+				return new [] { new Git(path) };
+			}
+
+			return directories
+				.Select(getAllGit)
+				.SelectMany(d => d)
+				.ToArray();
+		}
+	}
+}
